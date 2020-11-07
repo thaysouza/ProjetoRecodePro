850c277fbf4d96e5428f8b601aded6eab20dc70a
unknown <tayanesilvasouzas@gmail.com>
2020-11-06 23:58:15 -0300

    site com bootstrap

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Full Stack Eletro</title>
-        <link rel="stylesheet" href="./css/estilos.css">
+        <link rel="stylesheet" href="./css/estilo.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     </head>
   
--- produtos.php
+++ produtos.php
@@ -19,7 +19,7 @@ if(!$conn){
     <head>
         <meta charset="UTF-8">
         <title>Produtos - Full Stack Eletro</title>
-        <link rel="stylesheet" href="./css/estilos.css">
+        <link rel="stylesheet" href="./css/estilo.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <script src="./js/funcoes.js"></script>
     </head>
